889832a1b0b7cad7c9b0a5c4f4a18c5472405518
Neil Martin <neil.martin@jamf.com>
2026-02-14 14:49:08 +0000

    refactor: implement CRUD operations for AnalyticResource

--- internal/resources/analytic/resource.go
+++ internal/resources/analytic/resource.go
@@ -6,9 +6,6 @@ package analytic
 import (
 	"context"
 	"fmt"
-	"time"
-
-	common "github.com/smithjw/terraform-provider-jamfprotect/internal/common/helpers"
 
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
@@ -21,7 +18,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 
 	"github.com/smithjw/terraform-provider-jamfprotect/internal/client"
 )
@@ -185,156 +181,6 @@ func (r *AnalyticResource) Configure(ctx context.Context, req resource.Configure
 		return
 	}
 	r.client = client
-}
-
-// ---------------------------------------------------------------------------
-// CRUD
-// ---------------------------------------------------------------------------
-
-func (r *AnalyticResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data AnalyticResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	createTimeout, diags := data.Timeouts.Create(ctx, 30*time.Second)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	ctx, cancel := context.WithTimeout(ctx, createTimeout)
-	defer cancel()
-
-	vars := r.buildVariables(ctx, data, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	var result struct {
-		CreateAnalytic analyticAPIModel `json:"createAnalytic"`
-	}
-	if err := r.client.Query(ctx, createAnalyticMutation, vars, &result); err != nil {
-		resp.Diagnostics.AddError("Error creating analytic", err.Error())
-		return
-	}
-
-	r.apiToState(ctx, &data, result.CreateAnalytic, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	tflog.Trace(ctx, "created analytic", map[string]any{"uuid": data.ID.ValueString()})
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
-}
-
-func (r *AnalyticResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data AnalyticResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	readTimeout, diags := data.Timeouts.Read(ctx, 30*time.Second)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	ctx, cancel := context.WithTimeout(ctx, readTimeout)
-	defer cancel()
-
-	vars := map[string]any{"uuid": data.ID.ValueString()}
-	var result struct {
-		GetAnalytic *analyticAPIModel `json:"getAnalytic"`
-	}
-	if err := r.client.Query(ctx, getAnalyticQuery, vars, &result); err != nil {
-		resp.Diagnostics.AddError("Error reading analytic", err.Error())
-		return
-	}
-	if result.GetAnalytic == nil {
-		resp.State.RemoveResource(ctx)
-		return
-	}
-
-	r.apiToState(ctx, &data, *result.GetAnalytic, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
-}
-
-func (r *AnalyticResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data AnalyticResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// UUID comes from state, not plan.
-	var state AnalyticResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	data.ID = state.ID
-
-	updateTimeout, diags := data.Timeouts.Update(ctx, 30*time.Second)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	ctx, cancel := context.WithTimeout(ctx, updateTimeout)
-	defer cancel()
-
-	vars := r.buildVariables(ctx, data, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	vars["uuid"] = data.ID.ValueString()
-
-	var result struct {
-		UpdateAnalytic analyticAPIModel `json:"updateAnalytic"`
-	}
-	if err := r.client.Query(ctx, updateAnalyticMutation, vars, &result); err != nil {
-		resp.Diagnostics.AddError("Error updating analytic", err.Error())
-		return
-	}
-
-	r.apiToState(ctx, &data, result.UpdateAnalytic, &resp.Diagnostics)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
-}
-
-func (r *AnalyticResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data AnalyticResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	deleteTimeout, diags := data.Timeouts.Delete(ctx, 30*time.Second)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	ctx, cancel := context.WithTimeout(ctx, deleteTimeout)
-	defer cancel()
-
-	vars := map[string]any{"uuid": data.ID.ValueString()}
-	if err := r.client.Query(ctx, deleteAnalyticMutation, vars, nil); err != nil {
-		if common.IsNotFoundError(err) {
-			tflog.Trace(ctx, "analytic already deleted", map[string]any{"uuid": data.ID.ValueString()})
-			return
-		}
-		resp.Diagnostics.AddError("Error deleting analytic", err.Error())
-		return
-	}
-
-	tflog.Trace(ctx, "deleted analytic", map[string]any{"uuid": data.ID.ValueString()})
 }
 
 func (r *AnalyticResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
