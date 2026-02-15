73b845c2c980969520c98a5a760085bf111bc11b
Neil Martin <neil.martin@jamf.com>
2026-02-15 10:54:10 +0000

    Refactor prevent list resource to custom prevent list

--- internal/resources/custom_prevent_list/types.go
+++ internal/resources/custom_prevent_list/types.go
@@ -1,7 +1,7 @@
 // Copyright (c) James Smith 2025
 // SPDX-License-Identifier: MPL-2.0
 
-package prevent_list
+package custom_prevent_list
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
