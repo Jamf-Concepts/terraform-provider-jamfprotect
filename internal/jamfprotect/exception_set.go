24fd4e2440eeee4d424d2b7a09da6dc73edaab8e
Neil Martin <neil.martin@jamf.com>
2026-02-15 10:34:32 +0000

    style: format GraphQL fragments and mutations for consistency

--- internal/jamfprotect/exception_set.go
+++ internal/jamfprotect/exception_set.go
@@ -8,122 +8,122 @@ import "context"
 // exceptionSetFields defines the GraphQL fragment for exception set fields.
 const exceptionSetFields = `
 fragment ExceptionSetFields on ExceptionSet {
-  uuid
-  name
-  description
-  exceptions @skip(if: $minimal) {
-    type
-    value
-    appSigningInfo {
-      appId
-      teamId
-    }
-    ignoreActivity
-    analyticTypes
-		analytic @include(if: $RBAC_Analytic) {
-			name
-			uuid
+	uuid
+	name
+	description
+	exceptions @skip(if: $minimal) {
+		type
+		value
+		appSigningInfo {
+			appId
+			teamId
 		}
-  }
-  esExceptions @skip(if: $minimal) {
-    type
-    value
-    appSigningInfo {
-      appId
-      teamId
-    }
-    ignoreActivity
-    ignoreListType
-    ignoreListSubType
-    eventType
-  }
-  created
-  updated
-  managed
+		ignoreActivity
+		analyticTypes
+			analytic @include(if: $RBAC_Analytic) {
+				name
+				uuid
+		}
+	}
+	esExceptions @skip(if: $minimal) {
+		type
+		value
+		appSigningInfo {
+			appId
+			teamId
+		}
+		ignoreActivity
+		ignoreListType
+		ignoreListSubType
+		eventType
+	}
+	created
+	updated
+	managed
 }
 `
 
 // createExceptionSetMutation defines the GraphQL mutation for creating an exception set.
 const createExceptionSetMutation = `
 mutation createExceptionSet(
-  $name: String!,
-  $description: String,
-  $exceptions: [ExceptionInput!]!,
-  $esExceptions: [EsExceptionInput!]!,
-  $minimal: Boolean!,
-	$RBAC_Analytic: Boolean!
+	$name: String!,
+	$description: String,
+	$exceptions: [ExceptionInput!]!,
+	$esExceptions: [EsExceptionInput!]!,
+	$minimal: Boolean!,
+		$RBAC_Analytic: Boolean!
 ) {
-  createExceptionSet(input: {
-    name: $name,
-    description: $description,
-    exceptions: $exceptions,
-    esExceptions: $esExceptions
-  }) {
-    ...ExceptionSetFields
-  }
+	createExceptionSet(input: {
+		name: $name,
+		description: $description,
+		exceptions: $exceptions,
+		esExceptions: $esExceptions
+	}) {
+		...ExceptionSetFields
+	}
 }
 ` + exceptionSetFields
 
 // getExceptionSetQuery defines the GraphQL query for retrieving an exception set.
 const getExceptionSetQuery = `
 query getExceptionSet(
-  $uuid: ID!,
-  $minimal: Boolean!,
-	$RBAC_Analytic: Boolean!
+	$uuid: ID!,
+	$minimal: Boolean!,
+		$RBAC_Analytic: Boolean!
 ) {
-  getExceptionSet(uuid: $uuid) {
-    ...ExceptionSetFields
-  }
+	getExceptionSet(uuid: $uuid) {
+		...ExceptionSetFields
+	}
 }
 ` + exceptionSetFields
 
 // updateExceptionSetMutation defines the GraphQL mutation for updating an exception set.
 const updateExceptionSetMutation = `
 mutation updateExceptionSet(
-  $uuid: ID!,
-  $name: String!,
-  $description: String,
-  $exceptions: [ExceptionInput!]!,
-  $esExceptions: [EsExceptionInput!]!,
-  $minimal: Boolean!,
-	$RBAC_Analytic: Boolean!
+	$uuid: ID!,
+	$name: String!,
+	$description: String,
+	$exceptions: [ExceptionInput!]!,
+	$esExceptions: [EsExceptionInput!]!,
+	$minimal: Boolean!,
+		$RBAC_Analytic: Boolean!
 ) {
-  updateExceptionSet(uuid: $uuid, input: {
-    name: $name,
-    description: $description,
-    exceptions: $exceptions,
-    esExceptions: $esExceptions
-  }) {
-    ...ExceptionSetFields
-  }
+	updateExceptionSet(uuid: $uuid, input: {
+		name: $name,
+		description: $description,
+		exceptions: $exceptions,
+		esExceptions: $esExceptions
+	}) {
+		...ExceptionSetFields
+	}
 }
 ` + exceptionSetFields
 
 // deleteExceptionSetMutation defines the GraphQL mutation for deleting an exception set.
 const deleteExceptionSetMutation = `
 mutation deleteExceptionSet($uuid: ID!) {
-  deleteExceptionSet(uuid: $uuid) {
-    uuid
-  }
+	deleteExceptionSet(uuid: $uuid) {
+		uuid
+	}
 }
 `
 
 // listExceptionSetsQuery defines the GraphQL query for listing exception sets.
 const listExceptionSetsQuery = `
 query listExceptionSets($nextToken: String, $direction: OrderDirection = DESC, $field: ExceptionSetOrderField = created) {
-  listExceptionSets(
-    input: {next: $nextToken, order: {direction: $direction, field: $field}, pageSize: 100}
-  ) {
-    items {
-      uuid
-      name
-      managed
-    }
-    pageInfo {
-      next
-      total
-    }
-  }
+	listExceptionSets(
+		input: {next: $nextToken, order: {direction: $direction, field: $field}, pageSize: 100}
+	) {
+		items {
+			uuid
+			name
+			managed
+		}
+		pageInfo {
+			next
+			total
+		}
+	}
 }
 `
 
