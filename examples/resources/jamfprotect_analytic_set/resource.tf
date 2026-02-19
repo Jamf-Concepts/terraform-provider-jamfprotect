adb5848b7472f4ba4b174fd56ca03d4a51a64e6e
Neil Martin <neilmartin83@icloud.com>
2026-02-19 08:03:57 +0000

    docs: renaming/tweaks/removing basic examples

--- examples/resources/jamfprotect_analytic_set/resource.tf
+++
@@ -1,8 +0,0 @@
-resource "jamfprotect_analytic_set" "example" {
-  analytics = [
-    "3c8a88ef-277a-4238-a695-ebaa6eee0921",
-    "dcb69719-5ae1-46f6-972a-da1799daa00c",
-  ]
-  description = "Managed by Terraform"
-  name        = "Example Analytic Set"
-}
--- examples/resources/jamfprotect_custom_prevent_list/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "jamfprotect_custom_prevent_list" "example" {
-  description  = "Managed by Terraform"
-  list_data    = ["EXAMPLE"]
-  name         = "Example"
-  prevent_type = "Team ID"
-}
