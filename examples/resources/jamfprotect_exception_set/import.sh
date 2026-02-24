88ab74a4b39d19c86c486640792f94fc5261a660
Neil Martin <neil.martin@jamf.com>
2026-02-24 15:56:50 +0000

    chore: Update copyright information to Jamf Software LLC 2026 and add SPDX license identifier MPL-2.0 across multiple files and regenerate docs

--- examples/resources/jamfprotect_exception_set/import.sh
+++ examples/resources/jamfprotect_exception_set/import.sh
@@ -1,1 +1,4 @@
+# Copyright Jamf Software LLC 2026
+# SPDX-License-Identifier: MPL-2.0
+
 terraform import jamfprotect_exception_set.example "<exception-set-id>"
--- internal/resources/removable_storage_control_set/data_source.go
+++ internal/resources/removable_storage_control_set/data_source.go
@@ -1,3 +1,6 @@
+// Copyright Jamf Software LLC 2026
+// SPDX-License-Identifier: MPL-2.0
+
 package removable_storage_control_set
 
 import (
